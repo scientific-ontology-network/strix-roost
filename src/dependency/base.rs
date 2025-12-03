0426e4de20aa7a63de73c26c3023197933eca72c
Martin Glauer <martinglauer89@gmail.com>
2025-12-03 14:27:50 +0100

    Avoid cloning structures

--- src/dependency/base.rs
+++ src/dependency/base.rs
@@ -15,33 +15,32 @@ use itertools::Itertools;
 
 /// Represents a symbol in an ontology, which can be either a class expression or a role.
 #[derive(Debug, Eq, Clone, Hash, PartialEq)]
-pub enum OntologySymbol<T: ForIRI> {
+pub enum OntologySymbol<'a, T: ForIRI> {
     /// A reference to a class expression
-    CE(ClassExpression<T>),
+    CE(&'a ClassExpression<T>),
     /// A reference to an object property expression (role)
-    Role(ObjectPropertyExpression<T>),
+    Role(&'a ObjectPropertyExpression<T>),
 }
 
 /// Represents a dependency relationship between two ontology symbols
-pub type DependencyPair<T: ForIRI> = (OntologySymbol<T>, OntologySymbol<T>);
+pub type DependencyPair<'a, T: ForIRI> = (OntologySymbol<'a, T>, OntologySymbol<'a, T>);
 
 /// Maps ontology symbols to their dependent symbols
-pub type DependencyMap<T: ForIRI> = HashMap<OntologySymbol<T>, HashSet<OntologySymbol<T>>>;
+pub type DependencyMap<'a, T: ForIRI> = HashMap<OntologySymbol<'a, T>, HashSet<OntologySymbol<'a, T>>>;
 
 /// Trait for building dependency relationships between ontological components
-pub trait DependencyBuilder<T: ForIRI> {
+pub trait DependencyBuilder<'a, T: ForIRI> {
     /// Constructs a dependency map from an iterator of annotated components
     /// 
     /// # Arguments
     /// * `ontology_iter` - An iterator over annotated ontology components
-    fn build_dependencies<'a>(
+    fn build_dependencies(
         ontology_iter: impl Iterator<Item = &'a AnnotatedComponent<T>>,
-    ) -> DependencyMap<T>
-    where T:'a ;
+    ) -> DependencyMap<'a, T>;
 }
 
 /// Trait for analyzing syntax-based dependencies in ontological components
-pub trait SyntaxBasedDependency<T: ForIRI>: DependencyBuilder<T> {
+pub trait SyntaxBasedDependency<'a, T: ForIRI>: DependencyBuilder<'a, T> {
     /// Extracts dependency pairs from ontology components based on their syntactic structure
     /// 
     /// # Arguments
@@ -49,11 +48,11 @@ pub trait SyntaxBasedDependency<T: ForIRI>: DependencyBuilder<T> {
     ///
     /// # Returns
     /// A vector of dependency pairs representing relationships between ontological elements
-    fn dependencies_from_components<'a>(
+    fn dependencies_from_components(
         ontology_iter: impl Iterator<Item = &'a AnnotatedComponent<T>>,
-    ) -> Vec<DependencyPair<T>> where T:'a{
+    ) -> HashSet<DependencyPair<'a, T>>{
         ontology_iter
-            .map(|ce| match &ce.component {
+            .flat_map(|ce| match &ce.component {
                 Component::SubClassOf(ref sco) => Self::dependency_from_subsumption(sco),
                 Component::EquivalentClasses(ref ecs) => Self::dependency_from_equivalences(ecs),
                 Component::DisjointClasses(ref dcs) => Self::dependency_from_disjoint_classes(dcs),
@@ -140,9 +139,8 @@ pub trait SyntaxBasedDependency<T: ForIRI>: DependencyBuilder<T> {
                 Component::AnnotationPropertyRange(ref apr) => {
                     Self::dependency_from_annotation_property_range(apr)
                 }
-                _ => Vec::new(),
+                _ => HashSet::new(),
             })
-            .flatten()
             .collect()
     }
 
@@ -151,15 +149,15 @@ pub trait SyntaxBasedDependency<T: ForIRI>: DependencyBuilder<T> {
     // and returns a vector of dependency pairs.
 
     /// Extracts dependencies from subsumption relationships (SubClassOf axioms)
-    fn dependency_from_subsumption(_sco: &SCO<T>) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    fn dependency_from_subsumption(_sco: &'a SCO<T>) -> HashSet<DependencyPair<'a, T>> {
+        HashSet::new()
     }
 
-    fn dependency_from_equivalences(_ecs: &EquivalentClasses<T>) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    fn dependency_from_equivalences(_ecs: &'a EquivalentClasses<T>) -> HashSet<DependencyPair<'a,T>> {
+        HashSet::new()
     }
 
-    fn dependency_from_disjoint_classes(dcs: &DisjointClasses<T>) -> Vec<DependencyPair<T>> {
+    fn dependency_from_disjoint_classes(dcs: &'a DisjointClasses<T>) -> HashSet<DependencyPair<T>> {
         dcs.0
             .iter()
             .combinations(2)
@@ -171,179 +169,179 @@ pub trait SyntaxBasedDependency<T: ForIRI>: DependencyBuilder<T> {
             .collect()
     }
 
-    fn dependency_from_disjoint_class_pair<'a>(
-        _c1: &ClassExpression<T>,
-        _c2: &ClassExpression<T>,
-    ) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    fn dependency_from_disjoint_class_pair(
+        _c1: &'a ClassExpression<T>,
+        _c2: &'a ClassExpression<T>,
+    ) -> HashSet<DependencyPair<'a, T>> {
+        HashSet::new()
     }
 
-    fn dependency_from_disjoint_union(_du: &DisjointUnion<T>) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    fn dependency_from_disjoint_union(_du: &DisjointUnion<T>) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
-    fn dependency_from_sub_object_property(_spo: &SubObjectPropertyOf<T>) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    fn dependency_from_sub_object_property(_spo: &SubObjectPropertyOf<T>) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
     fn dependency_from_equiv_object_properties(
         _eops: &EquivalentObjectProperties<T>,
-    ) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    ) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
     fn dependency_from_disjoint_object_properties(
         _dops: &DisjointObjectProperties<T>,
-    ) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    ) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
     fn dependency_from_inverse_object_properties(
         _iop: &InverseObjectProperties<T>,
-    ) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    ) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
     fn dependency_from_object_property_domain(
         _opd: &ObjectPropertyDomain<T>,
-    ) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    ) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
-    fn dependency_from_object_property_range(_opr: &ObjectPropertyRange<T>) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    fn dependency_from_object_property_range(_opr: &ObjectPropertyRange<T>) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
     fn dependency_from_functional_object_property(
         _fop: &FunctionalObjectProperty<T>,
-    ) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    ) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
     fn dependency_from_inverse_functional_object_property(
         _ifop: &InverseFunctionalObjectProperty<T>,
-    ) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    ) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
     fn dependency_from_reflexive_object_property(
         _rop: &ReflexiveObjectProperty<T>,
-    ) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    ) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
     fn dependency_from_irreflexive_object_property(
         _irop: &IrreflexiveObjectProperty<T>,
-    ) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    ) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
     fn dependency_from_symmetric_object_property(
         _sop: &SymmetricObjectProperty<T>,
-    ) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    ) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
     fn dependency_from_asymmetric_object_property(
         _aop: &AsymmetricObjectProperty<T>,
-    ) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    ) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
     fn dependency_from_transitive_object_property(
         _top: &TransitiveObjectProperty<T>,
-    ) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    ) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
-    fn dependency_from_sub_data_property(_sdp: &SubDataPropertyOf<T>) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    fn dependency_from_sub_data_property(_sdp: &SubDataPropertyOf<T>) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
     fn dependency_from_equiv_data_properties(
         _edp: &EquivalentDataProperties<T>,
-    ) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    ) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
     fn dependency_from_disjoint_data_properties(
         _ddp: &DisjointDataProperties<T>,
-    ) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    ) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
-    fn dependency_from_data_property_domain(_dpd: &DataPropertyDomain<T>) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    fn dependency_from_data_property_domain(_dpd: &DataPropertyDomain<T>) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
-    fn dependency_from_data_property_range(_dpr: &DataPropertyRange<T>) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    fn dependency_from_data_property_range(_dpr: &DataPropertyRange<T>) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
     fn dependency_from_functional_data_property(
         _fdp: &FunctionalDataProperty<T>,
-    ) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    ) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
-    fn dependency_from_class_assertion(_ca: &ClassAssertion<T>) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    fn dependency_from_class_assertion(_ca: &ClassAssertion<T>) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
     fn dependency_from_object_property_assertion(
         _opa: &ObjectPropertyAssertion<T>,
-    ) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    ) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
     fn dependency_from_negative_object_property_assertion(
         _nopa: &NegativeObjectPropertyAssertion<T>,
-    ) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    ) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
     fn dependency_from_data_property_assertion(
         _dpa: &DataPropertyAssertion<T>,
-    ) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    ) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
     fn dependency_from_negative_data_property_assertion(
         _ndpa: &NegativeDataPropertyAssertion<T>,
-    ) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    ) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
-    fn dependency_from_same_individual(_si: &SameIndividual<T>) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    fn dependency_from_same_individual(_si: &SameIndividual<T>) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
-    fn dependency_from_different_individuals(_di: &DifferentIndividuals<T>) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    fn dependency_from_different_individuals(_di: &DifferentIndividuals<T>) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
-    fn dependency_from_annotation_assertion(_aa: &AnnotationAssertion<T>) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    fn dependency_from_annotation_assertion(_aa: &AnnotationAssertion<T>) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
     fn dependency_from_sub_annotation_property(
         _sapo: &SubAnnotationPropertyOf<T>,
-    ) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    ) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
     fn dependency_from_annotation_property_domain(
         _apd: &AnnotationPropertyDomain<T>,
-    ) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    ) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
     fn dependency_from_annotation_property_range(
         _apr: &AnnotationPropertyRange<T>,
-    ) -> Vec<DependencyPair<T>> {
-        Vec::new()
+    ) -> HashSet<DependencyPair<T>> {
+        HashSet::new()
     }
 
     /// Analyzes and extracts dependencies from a class expression
-    fn dependencies_from_class_expression(ce: &ClassExpression<T>) -> Vec<DependencyPair<T>>;
+    fn dependencies_from_class_expression(ce: &ClassExpression<T>) -> HashSet<DependencyPair<T>>;
 }
--- src/dependency/growth.rs
+++ src/dependency/growth.rs
@@ -10,21 +10,21 @@ impl GrowthDependency {
 
     fn build_super_map<'a, T: ForIRI>(
         ontology_iter: impl Iterator<Item=&'a AnnotatedComponent<T>>,
-    ) -> HashMap<OntologySymbol<T>, HashSet<OntologySymbol<T>>>
-    where T: 'a {
+    ) -> HashMap<OntologySymbol<'a, T>, HashSet<OntologySymbol<'a, T>>>
+    where {
         let mut sup_map = DependencyMap::new();
         for ax in ontology_iter {
             match &ax.component {
                 Component::SubClassOf(sco) => {
-                    sup_map.entry(OntologySymbol::CE(sco.sub.clone())).or_insert(HashSet::new()).insert(OntologySymbol::CE(sco.sup.clone()));
+                    sup_map.entry(OntologySymbol::CE(&sco.sub)).or_insert(HashSet::new()).insert(OntologySymbol::CE(&sco.sup));
                 }
                 Component::EquivalentClasses(EquivalentClasses(ecs)) => {
                     for a in ecs {
                         for b in ecs {
                             if (a != b)
                             {
-                                sup_map.entry(OntologySymbol::CE(a.clone())).or_insert(HashSet::new()).insert(OntologySymbol::CE(b.clone()));
-                                sup_map.entry(OntologySymbol::CE(b.clone())).or_insert(HashSet::new()).insert(OntologySymbol::CE(a.clone()));
+                                sup_map.entry(OntologySymbol::CE(a)).or_insert(HashSet::new()).insert(OntologySymbol::CE(b));
+                                sup_map.entry(OntologySymbol::CE(b)).or_insert(HashSet::new()).insert(OntologySymbol::CE(a));
                             }
                         }
                     }
@@ -32,7 +32,7 @@ impl GrowthDependency {
                 Component::SubObjectPropertyOf(sco) => {
                     match &sco.sub {
                         SubObjectPropertyExpression::ObjectPropertyChain(_) => {}
-                        SubObjectPropertyExpression::ObjectPropertyExpression(ope) => {sup_map.entry(OntologySymbol::Role(ope.clone())).or_insert(HashSet::new()).insert(OntologySymbol::Role(sco.sup.clone()));}
+                        SubObjectPropertyExpression::ObjectPropertyExpression(ope) => {sup_map.entry(OntologySymbol::Role(ope)).or_insert(HashSet::new()).insert(OntologySymbol::Role(&sco.sup));}
                     }
 
                 }
@@ -41,8 +41,8 @@ impl GrowthDependency {
                         for b in ecs {
                             if (a != b)
                             {
-                                sup_map.entry(OntologySymbol::Role(a.clone())).or_insert(HashSet::new()).insert(OntologySymbol::Role(b.clone()));
-                                sup_map.entry(OntologySymbol::Role(b.clone())).or_insert(HashSet::new()).insert(OntologySymbol::Role(a.clone()));
+                                sup_map.entry(OntologySymbol::Role(a)).or_insert(HashSet::new()).insert(OntologySymbol::Role(b));
+                                sup_map.entry(OntologySymbol::Role(b)).or_insert(HashSet::new()).insert(OntologySymbol::Role(a));
                             }
                         }
                     }
@@ -54,42 +54,57 @@ impl GrowthDependency {
         sup_map
     }
 
-    pub fn remove_supers<'a, T: ForIRI> (dep_map: DependencyMap<T>, ontology_iter: impl Iterator<Item = &'a AnnotatedComponent<T>>) -> DependencyMap<T> where T: 'a {
+    pub fn remove_supers<'a, T: ForIRI>(dep_map: DependencyMap<'a, T>, ontology_iter: impl Iterator<Item=&'a AnnotatedComponent<T>>) -> DependencyMap<'a, T>
+    where
+        T: 'a,
+    {
         let sup_map = transitive_closure(&Self::build_super_map(ontology_iter));
-        let supers = |depends_on: &HashSet<OntologySymbol<T>>| depends_on.into_iter().filter_map(|v|sup_map.get(v)).flatten().cloned().collect::<HashSet<OntologySymbol<T>>>();
-        HashMap::from_iter(dep_map.iter().map(|(k,v)| (k.clone(), v.difference(&supers(v).union(sup_map.get(k).unwrap_or(&HashSet::new())).cloned().collect()).cloned().collect())))
+        let mut new_map: DependencyMap<'a, T> = HashMap::new();
+        for (k, v) in dep_map.iter() {
+            let supers_of_classes_in_v = v.into_iter().filter_map(|x| sup_map.get(x)).flatten().collect::<HashSet<_>>();
+            let supers_of_k = match sup_map.get(k) {
+                None => HashSet::new(),
+                Some(k_supers) => k_supers.iter().map(|x| x).collect()
+            };
+            let irrelevant_dependencies = supers_of_classes_in_v.union(&supers_of_k).map(|v| *v).collect();
+            let v_set: HashSet<_> = v.iter().map(|x| x).collect();
+            let relevant_dependencies: HashSet<_> = v_set.difference(&irrelevant_dependencies).collect();
+            new_map.insert(k.clone(), relevant_dependencies.iter().map(|v| (**v).clone()).collect());
+        }
+        new_map
     }
 }
 
-impl<T: ForIRI> DependencyBuilder<T> for GrowthDependency {
-    fn build_dependencies<'a>(
+impl<'a, T: ForIRI> DependencyBuilder<'a, T> for GrowthDependency {
+    fn build_dependencies(
         ontology_iter: impl Iterator<Item = &'a AnnotatedComponent<T>>,
-    ) -> DependencyMap<T>
-    where T: 'a
+    ) -> DependencyMap<'a, T>
     {
         let mut map = DependencyMap::new();
         for (a,b) in Self::dependencies_from_components(ontology_iter){
             map.entry(a).or_insert(HashSet::new()).insert(b);
         }
-        transitive_closure(&map)
+        println!("Build transitive closure of growth dependency map... ");
+        transitive_closure(&map);
+        println!("Done!");
+        map
     }
 }
 
-impl<T: ForIRI> SyntaxBasedDependency<T> for GrowthDependency {
-    fn dependency_from_subsumption(_sco: &SubClassOf<T>) -> Vec<DependencyPair<T>> {
-        let a: Vec<DependencyPair<T>> =
-            vec![(OntologySymbol::CE(_sco.sub.clone()), OntologySymbol::CE(_sco.sup.clone()))];
-        let b: Vec<DependencyPair<T>> = Self::dependencies_from_class_expression(&_sco.sub);
-        let c: Vec<DependencyPair<T>> = Self::dependencies_from_class_expression(&_sco.sup);
+impl<'a, T: ForIRI> SyntaxBasedDependency<'a, T> for GrowthDependency {
+    fn dependency_from_subsumption(_sco: &SubClassOf<T>) -> HashSet<DependencyPair<T>> {
+        let a: HashSet<DependencyPair<T>> = [(OntologySymbol::CE(&_sco.sub), OntologySymbol::CE(&_sco.sup))].into();
+        let b: HashSet<DependencyPair<T>> = Self::dependencies_from_class_expression(&_sco.sub);
+        let c: HashSet<DependencyPair<T>> = Self::dependencies_from_class_expression(&_sco.sup);
         a.into_iter().chain(b.into_iter().chain(c)).collect()
     }
 
-    fn dependencies_from_class_expression(ce: &ClassExpression<T>) -> Vec<DependencyPair<T>> {
+    fn dependencies_from_class_expression(ce: &ClassExpression<T>) -> HashSet<DependencyPair<T>> {
         match ce {
             ClassExpression::ObjectIntersectionOf(exprs) => exprs
                 .into_iter()
                 .flat_map(|ce2| {
-                    [(OntologySymbol::CE(ce.clone()), OntologySymbol::CE(ce2.clone()))]
+                    [(OntologySymbol::CE(ce), OntologySymbol::CE(ce2))]
                         .into_iter()
                         .chain(Self::dependencies_from_class_expression(ce2))
                 })
@@ -97,19 +112,19 @@ impl<T: ForIRI> SyntaxBasedDependency<T> for GrowthDependency {
             ClassExpression::ObjectUnionOf(exprs) => exprs
                 .into_iter()
                 .flat_map(|ce2| {
-                    [(OntologySymbol::CE(ce2.clone()), OntologySymbol::CE(ce.clone()))]
+                    [(OntologySymbol::CE(ce2), OntologySymbol::CE(ce))]
                         .into_iter()
                         .chain(Self::dependencies_from_class_expression(ce2))
                 })
                 .collect(),
             ClassExpression::ObjectSomeValuesFrom { ope, bce } => [
-                (OntologySymbol::CE(ce.clone()), OntologySymbol::CE(*bce.clone())),
-                (OntologySymbol::CE(ce.clone()), OntologySymbol::Role(ope.clone())),
+                (OntologySymbol::CE(ce), OntologySymbol::CE(bce)),
+                (OntologySymbol::CE(ce), OntologySymbol::Role(ope)),
             ]
                 .into_iter()
                 .chain(Self::dependencies_from_class_expression(bce))
                 .collect(),
-            _ => Vec::new(),
+            _ => HashSet::new(),
         }
     }
 }
