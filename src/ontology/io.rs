c84e5abc5a29f1233f13e5b1be70fdc1ba7c0511
Martin Glauer <martinglauer89@gmail.com>
2025-12-19 10:14:21 +0100

    Remove unused import

--- src/ontology/io.rs
+++ src/ontology/io.rs
@@ -4,7 +4,6 @@ use horned_owl::model::*;
 use horned_owl::ontology::set::SetOntology;
 use std::fs::File;
 use std::io::BufReader;
-use horned_owl::io::rdf::reader::ConcreteRDFOntology;
 
 pub fn load_set_ontology(path: &str) -> Result<SetOntology<ArcStr>, StrixError> {
     let ending = path.split(".").last().unwrap();
@@ -38,7 +37,7 @@ fn load_rdf_ontology(path: &str) -> Result<SetOntology<ArcStr>, StrixError> {
 
 fn load_ofn_ontology(path: &str) -> Result<SetOntology<ArcStr>, StrixError> {
     let file = File::open(path)?;
-    let mut reader = BufReader::new(file);
+    let reader = BufReader::new(file);
     let build = Build::new_arc();
     let res=
         ofn::reader::read_with_build::<ArcStr, SetOntology<ArcStr>, BufReader<File>>(
