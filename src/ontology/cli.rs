b4437376daf1d9b23f97d5a098a841e5740196f4
Martin Glauer <martinglauer89@gmail.com>
2026-06-29 17:34:52 +0200

    Improve type-annotations

--- src/ontology/cli.rs
+++ src/ontology/cli.rs
@@ -4,8 +4,9 @@ use clap::Args;
 use horned_owl::model::{ForIRI, Literal};
 use horned_owl::ontology::set::SetOntology;
 use serde_json::{json, Value};
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
+use std::hash::RandomState;
 use std::io::BufWriter;
 
 // Extract labels and definitions
@@ -35,9 +36,9 @@ impl<T: ForIRI> Runnable<T> for AnnotationWriter {
             .keys()
             .chain(annotations.labels.keys())
             .collect();
-        let json_anno = json!(iris.into_iter().map(|k| (k.to_string(), json!({
+        let json_anno = json!(HashMap::<_,_,RandomState>::from_iter(iris.into_iter().map(|k| (k.to_string(), json!({
             "definitions": annotations.definitions.get(&k).unwrap_or(&Vec::new()).into_iter().map(render_literal).collect::<Vec<Value>>(),
-            "labels": annotations.labels.get(&k).unwrap_or(&Vec::new()).iter().map(render_literal).collect::<Vec<Value>>()}))).collect::<Vec<_>>());
+            "labels": annotations.labels.get(&k).unwrap_or(&Vec::new()).iter().map(render_literal).collect::<Vec<Value>>()})))));
         let file = File::create(self.out_path.clone()).expect("Failed to create file");
         let mut writer = BufWriter::new(file);
         serde_json::to_writer_pretty(&mut writer, &json_anno).expect("Failed to write JSON to file");
